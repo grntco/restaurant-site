2bad97b635e879de9ce59fc998c65492444873ce
Grant Collins <grantcollins03@gmail.com>
2023-05-23 18:29:17 -0400

    reset() in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import './styles.css';
 import loadHome from './modules/home';
+import loadContact from './modules/contact';
+
+loadContact();
 
 const content = document.getElementById('content');
-content.appendChild(loadHome());
+const homeBtn = document.getElementById('home-btn');
+
+const reset = () => {
+    content.innerHTML = '';
+}
+
+homeBtn.addEventListener('click', function() {
+    reset();
+    content.appendChild(loadHome());
+});
+
---
+++ src/modules/contact.js
@@ -0,0 +1,6 @@
+export default function loadContact() {
+    console.log('Hello from contact!');
+
+    const contactPage = document.createElement('div');
+    contactPage.classList.add('contact-page');
+}
