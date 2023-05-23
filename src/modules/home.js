756d8f7d41b634ef142190d85cff49de650d017e
Grant Collins <grantcollins03@gmail.com>
2023-05-23 17:23:14 -0400

    createCTAContainer() in home.js

--- src/modules/home.js
+++ src/modules/home.js
@@ -8,19 +8,43 @@ export default function loadHome() {
         const heroSection = document.createElement('div');
         heroSection.classList.add('hero-section');
 
-        const heroLogo = document.createElement('div');
-        heroLogo.classList.add('hero-logo');
-        heroLogo.innerHTML = `<img src="/src/images/hero-logo.png" alt="Grits & Griddles logo">`
+        const logo = document.createElement('div')
+        logo.classList.add('hero-logo');
+        logo.innerHTML = `<img src="/src/images/hero-logo.png" alt="Grits & Griddles logo">`
 
-        const heroHeading = document.createElement('h1');
-        heroHeading.classList.add('hero-heading');
-        heroHeading.innerHTML = '<span>All Things</span> Breakfast,<br>Served <span>All Day</span>';
+        const h1 = document.createElement('h1');
+        h1.classList.add('hero-heading');
+        h1.innerHTML = '<span>All Things</span> Breakfast,<br>Served <span>All Day</span>';
     
-        heroSection.appendChild(heroLogo);
-        heroSection.appendChild(heroHeading);
+        heroSection.appendChild(logo);
+        heroSection.appendChild(h1);
+        
         homePage.appendChild(heroSection);
     })();
 
+    const createCTA = (function() {
+        console.log('hello from CTA!');
+
+        const CTAContainer = document.createElement('div')
+        CTAContainer.classList.add('cta-container');
+
+        const h2 = document.createElement('h2');
+        h2.textContent = 'Hungry?';
+        
+        const p = document.createElement('p');
+        p.textContent = 'Come prepared! Pick out your order before dining with us.';
+
+        const btn = document.createElement('button');
+        btn.id = 'view-menu-btn';
+        btn.textContent = 'View Menu';
+
+        CTAContainer.appendChild(h2);
+        CTAContainer.appendChild(p);
+        CTAContainer.appendChild(btn);
+
+        // The Button may need an event listener here to open the menu page.
+        homePage.appendChild(CTAContainer);
+    })();
 
     return homePage;
 }
