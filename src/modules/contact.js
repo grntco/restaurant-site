c00783f164e1764c43021148893c148ee18aec54
Grant Collins <grantcollins03@gmail.com>
2023-05-24 14:33:09 -0400

    refactored contact.js

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -6,11 +6,11 @@ export default function loadContact() {
     h1.textContent = 'Contact Us';
 
     const createInfoContainer = (function() {
-        const infoContainer = document.createElement('div');
+        const infoContainer = contactPage.appendChild(document.createElement('div'));
         infoContainer.classList.add('info-container');
 
         const createContactInfoSection = (function() {
-            const contactInfoSection = document.createElement('div');
+            const contactInfoSection = infoContainer.appendChild(document.createElement('div'));
             contactInfoSection.classList.add('info-section');
 
             const h2 = contactInfoSection.appendChild(document.createElement('h2'));
@@ -21,12 +21,10 @@ export default function loadContact() {
 
             const email = contactInfoSection.appendChild(document.createElement('div'));
             email.textContent = 'howdy@gritsandgriddles.com';
-
-            infoContainer.appendChild(contactInfoSection);
         })();
 
         const createHoursSection = (function() {
-            const hoursSection = document.createElement('div');
+            const hoursSection = infoContainer.appendChild(document.createElement('div'));
             hoursSection.classList.add('info-section');
 
             const h2 = hoursSection.appendChild(document.createElement('h2'));
@@ -43,7 +41,7 @@ export default function loadContact() {
                 ];
 
                 for (let i = 0; i < days.length; i++) {
-                    const dayContainer = document.createElement('div');
+                    const dayContainer = hoursSection.appendChild(document.createElement('div'));
                     dayContainer.classList.add('day-container');
 
                     const day = dayContainer.appendChild(document.createElement('div'));
@@ -51,16 +49,12 @@ export default function loadContact() {
 
                     const hours = dayContainer.appendChild(document.createElement('div'));
                     hours.textContent = `${days[i].open}am – ${days[i].close}pm`;
-
-                    hoursSection.appendChild(dayContainer);
                 }
             })();
-
-            infoContainer.appendChild(hoursSection);
         })();
 
         const createLocationSection = (function() {
-            const locationSection = document.createElement('div');
+            const locationSection = infoContainer.appendChild(document.createElement('div'));
             locationSection.classList.add('info-section');
 
             const h2 = locationSection.appendChild(document.createElement('h2'));
@@ -74,25 +68,17 @@ export default function loadContact() {
 
             const ifOnlyText = locationSection.appendChild(document.createElement('div'));
             ifOnlyText.textContent = 'If only this place existed...';
-
-            infoContainer.appendChild(locationSection); 
-            // Alternatively, the final append childs could be called in a separate function, to remove IIFEs
-            // basically, just return the container you are creating, and call the function on the main element to create
         })();
-
-        contactPage.appendChild(infoContainer);
     })();
 
     const createMap = (function() {
-        const map = document.createElement('div');
+        const map = contactPage.appendChild(document.createElement('div'));
         map.classList.add('map');
 
         const embed = map.appendChild(document.createElement('iframe'));
         embed.src = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2891.4408197792063!2d-87.96818272283411!3d43.555696871106875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88049171727724d5%3A0x741e66e429a56866!2s551%20Krier%20Ln%2C%20Random%20Lake%2C%20WI%2053075!5e0!3m2!1sen!2sus!4v1684798541675!5m2!1sen!2sus'
         embed.style.border = "none";
         embed.referrerPolicy = 'no-referrer-when-downgrade';
-
-        contactPage.appendChild(map)
     })();
 
     return contactPage;
