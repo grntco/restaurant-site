bf3c6831470714db5402dab0b42276dbd575518b
Grant Collins <grantcollins03@gmail.com>
2023-05-24 14:26:18 -0400

    refactored home.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import loadMenu from './modules/menu';
 const content = document.getElementById('content');
 const homeBtn = document.getElementById('home-btn');
 const contactBtn = document.getElementById('contact-btn');
-
-content.appendChild(loadMenu());
+const menuBtn = document.getElementById('menu-btn');
+// const viewMenuBtn = document.getElementById('view-menu-btn');
 
 const reset = () => {
     content.innerHTML = '';
@@ -22,3 +22,17 @@ contactBtn.addEventListener('click', function() {
     reset();
     content.appendChild(loadContact());
 });
+
+menuBtn.addEventListener('click', function() {
+    reset();
+    content.appendChild(loadMenu());
+});
+
+// viewMenuBtn.addEventListener('click', function() {
+//     reset();
+//     content.appendChild(loadMenu());
+// });
+
+// DOESNT WORK?
+
+
--- src/modules/home.js
+++ src/modules/home.js
@@ -3,43 +3,33 @@ export default function loadHome() {
     homePage.classList.add('home-page');
 
     const createHeroSection = (function() {
-        const heroSection = document.createElement('div');
+        const heroSection = homePage.appendChild(document.createElement('div'));
         heroSection.classList.add('hero-section');
 
-        const logo = document.createElement('div')
+        const logo = heroSection.appendChild(document.createElement('div'));
         logo.classList.add('hero-logo');
         logo.innerHTML = `<img src="/src/images/hero-logo.png" alt="Grits & Griddles logo">`
 
-        const h1 = document.createElement('h1');
+        const h1 = heroSection.appendChild(document.createElement('h1'));
         h1.classList.add('hero-heading');
         h1.innerHTML = '<span>All Things</span> Breakfast,<br>Served <span>All Day</span>';
-    
-        heroSection.appendChild(logo);
-        heroSection.appendChild(h1);
-
-        homePage.appendChild(heroSection);
     })();
 
     const createCTA = (function() {
-        const CTAContainer = document.createElement('div')
+        const CTAContainer = homePage.appendChild(document.createElement('div'));
         CTAContainer.classList.add('cta-container');
 
-        const h2 = document.createElement('h2');
+        const h2 = CTAContainer.appendChild(document.createElement('h2'));
         h2.textContent = 'Hungry?';
         
-        const p = document.createElement('p');
+        const p = CTAContainer.appendChild(document.createElement('p'));
         p.textContent = 'Come prepared! Pick out your order before dining with us.';
 
-        const btn = document.createElement('button');
+        const btn = CTAContainer.appendChild(document.createElement('button'));
         btn.id = 'view-menu-btn';
         btn.textContent = 'View Menu';
 
-        CTAContainer.appendChild(h2);
-        CTAContainer.appendChild(p);
-        CTAContainer.appendChild(btn);
-
         // The Button may need an event listener here to open the menu page.
-        homePage.appendChild(CTAContainer);
     })();
 
     return homePage;
